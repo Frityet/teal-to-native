9af84b1d15ae75157a943fb6093306cadb62f521
Amrit Bhogal <ambhogal01@gmail.com>
2023-11-16 10:50:17 -0800

    Delete licence in preperation for template

--- check-lua.sh
+++ check-lua.sh
@@ -10,5 +10,4 @@ LUAOT_VER="$($LUAOT_LUA -v)"
 
 if [ "$LUA_VER" != "$LUAOT_VER" ]; then
     >&2 printf "\x1b[33mWarning: %s provided by host (%s) does not match LuaOT %s\n\x1b[0m\x1b[31mTHIS MAY CAUSE CRASHING\x1b[0m\n" "$LUA_VER" "$LUA" "$LUAOT_VER"
-
 fi
